22725fc4ee0002b52b07191fdccf5b913a217a41
Arjun Earthperon <mail@earthperson.org>
2024-10-29 17:23:18 -0400

    - port `bitwise` library from abandoned `scram-node` implementation

---
+++ src/bitwise/bit_ops.h
@@ -0,0 +1,12 @@
+#ifndef BITWISE_BITOPS_H
+#define BITWISE_BITOPS_H
+
+#include <cstdint>
+#include <bitset>
+#include <vector>
+
+#include "bar.h"
+
+bool kn(bit_index_t k, BIT_ARRAY* n);
+
+#endif //BITWISE_BITOPS_H
