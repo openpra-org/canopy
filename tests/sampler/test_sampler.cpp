6d0df3474a7eb8b489e05163d0e4f18e9b6bf98d
Arjun Earthperson <a.earthperson@openpra.org>
2025-01-01 17:09:57 -0500

    - implement bit-packed sampler, parallelized using openmp for now

--- tests/sampler/test_sampler.cpp
+++ tests/sampler/test_sampler.cpp
@@ -7,7 +7,7 @@
 
 BOOST_AUTO_TEST_SUITE(SamplerBitpackTests)
 
-BOOST_AUTO_TEST_CASE(BasicGenerationTest)
+BOOST_AUTO_TEST_CASE(BasicMultiBatchGenerationTest)
 {
     // Define probabilities
     // two events, 6 batches each
@@ -33,6 +33,31 @@ BOOST_AUTO_TEST_CASE(BasicGenerationTest)
     }
 }
 
+BOOST_AUTO_TEST_CASE(BasicBroadcastBatchGenerationTest)
+{
+    // Define probabilities
+    // two events
+    std::vector<float_t> probs = {0.3, 0.9};
+
+    // batch size is 5, sample size is 8
+    auto batch_size = 5;
+    auto sample_size = 8;
+
+    // Generate Bernoulli samples
+    auto packed_bits = canopy::random::sampler::generate_bernoulli<float_t, uint64_t>(probs, batch_size, sample_size);
+
+    // Output results
+    for (std::size_t i = 0; i < packed_bits.size(); ++i) {
+        for (std::size_t j = 0; j < packed_bits[i].size(); ++j) {
+            std::cout << "Event " << i << ", Batch " << j << ": ";
+            for (const auto& val : packed_bits[i][j]) {
+                std::cout << static_cast<uint64_t>(val) << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
 BOOST_AUTO_TEST_CASE(BasicLargeGenerationTest)
 {
     // Define probabilities
@@ -41,20 +66,9 @@ BOOST_AUTO_TEST_CASE(BasicLargeGenerationTest)
         {0.1}   // Probabilities for event 1
     };
 
-    std::size_t n_sample_packs_per_probability = 1e9;
+    std::size_t n_sample_packs_per_probability = 1e3;
 
     // Generate Bernoulli samples
     auto packed_bits = canopy::random::sampler::generate_bernoulli<double, uint8_t>(probs, n_sample_packs_per_probability);
-
-    // Output results
-    // for (std::size_t i = 0; i < packed_bits.size(); ++i) {
-    //     for (std::size_t j = 0; j < packed_bits[i].size(); ++j) {
-    //         // std::cout << "Event " << i << ", Batch " << j << ": ";
-    //         // for (const auto& val : packed_bits[i][j]) {
-    //         //     std::cout << static_cast<int>(val) << " ";
-    //         // }
-    //         // std::cout << std::endl;
-    //     }
-    // }
 }
 }
